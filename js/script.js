b2d21fd0fca9212d11b0ff9be86ec9f60862bdd9
Marcin Osojca <osojca.marcin1@gmail.com>
2024-12-01 18:49:02 +0100

    js logic added to control task list, css added

--- js/script.js
+++ js/script.js
@@ -2,51 +2,82 @@
 
     const taskTable = [
         {
-            object: "pierwsze zadanie do zrobienia",
+            content: "pierwsze zadanie do zrobienia",
             status: "done",
         },
         {
-            object: "drugie zadanie do zrobienia",
-            status: "in progress",
+            content: "drugie zadanie do zrobienia",
+            status: "toDo",
         },
     ];
+
+    const removeTask = (index) => {
+        taskTable.splice(index, 1);
+        render();
+    };
+
+    const taskDoneButton = (indexWTabeli) => {
+        taskTable[indexWTabeli].status === "done" ? taskTable[indexWTabeli].status = "toDo" : taskTable[indexWTabeli].status = "done";
+        render();
+    };
 
 
     const render = () => {
         let htmlString = "";
 
-        for (const taskItem of taskTable) {
+        for (const taskTableElement of taskTable) {
             htmlString += `
-                <li ${taskItem.status === "done" ? "class=\"task__done\"" : ""}>
-                ${taskItem.object}
+                <li ${taskTableElement.status === "done" ? "class=\"task__done\"" : "class=\"task\"" }>
+                <button class="doneButton js-doneButton"">ZROBIONE</button>
+                ${taskTableElement.content}
+                <button class="removeButton js-removeButton">USUŃ</button>
                  </li>
         `};
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-removeButton");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+
+            const toggleDoneButtons = document.querySelectorAll(".js-doneButton");
+
+            toggleDoneButtons.forEach((doneButton, index) => {
+                doneButton.addEventListener("click", () => {
+                    taskDoneButton(index);
+                });
+
+            });
+        });
+
     };
 
 
 
-    const addNewPosition = (newTaskContent) => {
+    const addNewTablePosition = (newTaskContent) => {
         taskTable.push({
-            object: newTaskContent,
-            status: "in progress",
+            content: newTaskContent,
+            status: "toDo",
         });
 
         render();
     };
 
-const onFormSubmit = (event) => {
+    const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-        console.log(newTaskContent);
+        //console.log(newTaskContent);
 
         if (newTaskContent === "") {
             document.querySelector(".js-newTask").focus();
             return;
         };
+        addNewTablePosition(newTaskContent);
+        console.log(taskTable);
 
-        addNewPosition(newTaskContent);
     };
 
 
